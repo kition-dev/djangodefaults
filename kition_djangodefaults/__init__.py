f35eed6b827bc50a0974d6d3fbb0044fcaca8dc4
Dennis Stritzke <dennis.stritzke@kition.dev>
2024-10-12 18:28:54 +0200

    Making ruff happy.

--- kition_djangodefaults/__init__.py
+++ kition_djangodefaults/__init__.py
@@ -1,1 +1,3 @@
-from .settings.initialize import initialize_default_settings as initialize_default_settings
+from .settings.initialize import (
+    initialize_default_settings as initialize_default_settings,
+)
